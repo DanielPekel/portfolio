1056b70c5323266d3186f1d7a15e9ef11d7e93f8
Daniël Pekel <danielpekel03@gmail.com>
2022-11-26 17:57:22 +0100

    front page first 2 segments made

--- index.php
+++ index.php
@@ -1,1 +1,91 @@
-<?php
+<?php require_once 'header.php'; ?>
+
+    <div class="vh-100">
+        <div class="container pt-4 pb-3">
+            <div class="row justify-content-center pt-5 pb-3">
+                <div class="col-8 col-md-3 mt-5 pt-5">
+                    <img src="./assets/media/selfie.jpg" class="rounded-circle" alt="A picture of myself" width="100%"
+                         height="auto">
+                </div>
+            </div>
+            <div class="pt-4 container text-center">
+                <h1 class="fs-1 fw-bold">Hi, I'm <span class="head-primary">Daniël</span></h1>
+                <p class="fs-5 mt-3 fw-bolder">I'm a <span class="head-primary">software developer</span> from the
+                    Netherlands</p>
+            </div>
+        </div>
+        <div class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-1 text-center">
+                    <a class="fs-1" href="https://www.linkedin.com/in/dani%C3%ABl-pekel-software-dev/"><i
+                                class="fa-brands fa-linkedin"></i></a>
+                </div>
+                <div class="col-12 col-md-1 text-center">
+                    <a class="fs-1 text-success" href="https://wa.me/684054534"><i
+                                class="fa-brands fa-whatsapp"></i></a>
+                </div>
+                <div class="col-12 col-md-1 text-center">
+                    <a class="fs-1" href="https://www.instagram.com/1oak_daniel/"><i id="insta"
+                                                                                     class="fa-brands fa-instagram"></i></a>
+                </div>
+                <div class="col-12 col-md-1 text-center">
+                    <a class="fs-1 text-white" href="mailto:danielpekel03@gmail.com"><i
+                                class="fa-solid fa-envelope"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="vh-100 pt-5">
+        <div class="container pt-5">
+            <div class="row pt-5 mt-5 justify-content-between">
+                <div class="col-12 pt-5 mt-5 col-md-4">
+                    <div class="topbar pt-1 px-3">
+                        <div class="row justify-content-between">
+                            <div class="col-5">
+                                <p>Github Portfolio</p>
+                            </div>
+                            <div class="col-4">
+                                <div class="container">
+                                    <div class="row">
+                                        <div class="col-4">
+                                            <i class="fa-solid fa-up-right-and-down-left-from-center"></i>
+                                        </div>
+                                        <div class="col-4">
+                                            <i class="fa-solid fa-minus"></i>
+                                        </div>
+                                        <div class="col-4">
+                                            <i class="fa-solid fa-xmark"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bottombar">
+                        <div class="container pt-3">
+                            <p><code><span class="text-yellow fw-bold">git</span> init</code></p>
+                            <p><code><span>git</span> add .</code></p>
+                            <p><code><span>git</span> remote add origin master <span class="text-white-50">https://github.com/DanielPekel/portfolio</span></code>
+                            </p>
+                            <p><code><span>git</span> commit <span class="text-white-50">-m</span> <span
+                                            class="text-blue">Pilot</span></code></p>
+                            <p><code><span>git</span> push</code></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 pt-5">
+                    <div class="container">
+                        <h2 class="text-white fw-bold pb-3">My projects</h2>
+                        <p class="text-white-50">All of my projects are visible in github. I'm trying to expirement as much as possible with
+                            all different sorts of programming languages so I'll keep learning new things and habits in
+                            coding.<br/><br/>
+                            One of my latest projects is this portfolio website. I'm now learning to make a one page
+                            website with as much necessary information for you.</p>
+                    </div>
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php require_once 'footer.php'; ?>
