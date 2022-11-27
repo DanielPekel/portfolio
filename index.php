2ef11da4d51cc3c41af7059586bee0f6d1918c66
Daniël Pekel <danielpekel03@gmail.com>
2022-11-27 22:53:20 +0100

    changes to mobile version and implementing skills and about me

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php require_once 'header.php'; ?>
 
-    <div class="vh-100">
-        <div class="container pt-4 pb-3">
-            <div class="row justify-content-center pt-5 pb-3">
-                <div class="col-8 col-md-3 mt-5 pt-5">
+    <div class="views">
+        <div class="container pt-md-4 pb-3">
+            <div class="row justify-content-center pt-md-5 pb-3">
+                <div class="col-8 text-start col-md-3 mt-5 pt-5">
                     <img src="./assets/media/selfie.jpg" class="rounded-circle" alt="A picture of myself" width="100%"
                          height="auto">
                 </div>
@@ -14,34 +14,34 @@
                     Netherlands</p>
             </div>
         </div>
-        <div class="container mt-5">
+        <div class="container mt-md-5 mt-2">
             <div class="row justify-content-center">
-                <div class="col-12 col-md-1 text-center">
+                <div class="col-3 col-md-1 text-center">
                     <a class="fs-1" href="https://www.linkedin.com/in/dani%C3%ABl-pekel-software-dev/"><i
                                 class="fa-brands fa-linkedin"></i></a>
                 </div>
-                <div class="col-12 col-md-1 text-center">
+                <div class="col-3 col-md-1 text-center">
                     <a class="fs-1 text-success" href="https://wa.me/684054534"><i
                                 class="fa-brands fa-whatsapp"></i></a>
                 </div>
-                <div class="col-12 col-md-1 text-center">
+                <div class="col-3 col-md-1 text-center">
                     <a class="fs-1" href="https://www.instagram.com/1oak_daniel/"><i id="insta"
                                                                                      class="fa-brands fa-instagram"></i></a>
                 </div>
-                <div class="col-12 col-md-1 text-center">
+                <div class="col-3 col-md-1 text-center">
                     <a class="fs-1 text-white" href="mailto:danielpekel03@gmail.com"><i
                                 class="fa-solid fa-envelope"></i></a>
                 </div>
             </div>
         </div>
     </div>
-    <div class="vh-100 pt-5">
+    <div class="views pt-md-5">
         <div class="container pt-5">
-            <div class="row pt-5 mt-5 justify-content-between">
+            <div class="row pt-md-5 mt-md-5 justify-content-between">
                 <div class="col-12 pt-5 mt-5 col-md-4">
                     <div class="topbar pt-1 px-3">
                         <div class="row justify-content-between">
-                            <div class="col-5">
+                            <div class="col-md-5 col-8">
                                 <p>Github Portfolio</p>
                             </div>
                             <div class="col-4">
@@ -74,18 +74,155 @@
                     </div>
                 </div>
                 <div class="col-12 col-md-6 pt-5">
-                    <div class="container">
+                    <div class="container mt-4">
                         <h2 class="text-white fw-bold pb-3">My projects</h2>
-                        <p class="text-white-50">All of my projects are visible in github. I'm trying to expirement as much as possible with
+                        <p class="text-white-50">All of my projects are visible in github. I'm trying to expirement as
+                            much as possible with
                             all different sorts of programming languages so I'll keep learning new things and habits in
                             coding.<br/><br/>
-                            One of my latest projects is this portfolio website. I'm now learning to make a one page
-                            website with as much necessary information for you.</p>
+                            One of my latest projects is this portfolio website. I'm now learning to make an one page
+                            website with as much necessary information for you.<br/><br/>
+                            If you maybe have requests for a website or an app, I'm always open for a chat and I'll try
+                            to get you
+                            to reach your goal.
+                        </p>
                     </div>
                     <h2></h2>
                 </div>
             </div>
         </div>
     </div>
-
+    <div class="views pt-md-5 pt-2">
+        <div class="container pt-md-5 pt-0 px-4">
+            <div class="row justify-content-between pt-md-5 mt-5">
+                <div class="col-12 col-md-8 text-start pt-5 mt-3">
+                    <h3 class="text-white fw-bold fs-1">About me</h3>
+                    <p class="text-white pt-4">
+                        I'm a 19-year-old software developer from the Netherlands. I'm currently studying at Deltion
+                        College in Zwolle.
+                        I'm currently in my second year of the MBO ICT programme. I'm learning a lot of new things
+                        about programming, and
+                        I'm trying to learn as much as possible.<br/><br/>
+                        I'm currently working at a small company called <a class="text-white attribute"
+                                                                           href="https://service-ict.nl">Service-ICT</a>,
+                        this is the place where I gained all my experience so far.<br/><br/>
+                    </p>
+                    <div class="container text-center">
+                        <p class="text-indigo fw-bold fs-4">"I'm hoping to become an experienced and
+                            acknowledged<br/>full stack software developer in the future."
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <img src="./assets/media/portfolioImage2.jpg" class="rounded-5" alt="A picture of myself"
+                         width="100%"
+                         height="auto">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="views pt-5">
+        <div class="container pt-5 text-center">
+            <h2 class="text-white fw-bold fs-1">My skills</h2>
+            <div class="row justify-content-center pt-md-5 mt-3 skillcards">
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-white fa-sharp fa-solid fa-share-nodes"></i></div>
+                            <div class="col-8 text-start"><h3>Socializing</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-white fa-solid fa-brain"></i></div>
+                            <div class="col-8 text-start"><h3>Innovative</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 fa-solid fa-user-group text-white"></i></div>
+                            <div class="col-8 text-start"><h3>Team-player</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 fa-regular text-white fa-thumbs-up"></i></div>
+                            <div class="col-8 text-start"><h3>Customer-friendly</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-white fa-regular fa-eye"></i></div>
+                            <div class="col-8 text-start"><h3>Focused</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-white fa-brands fa-github"></i></div>
+                            <div class="col-8 text-start"><h3>Github</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-orange fa-brands fa-html5"></i></div>
+                            <div class="col-8 text-start"><h3>HTML</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-blue fa-brands fa-css3-alt"></i></div>
+                            <div class="col-8 text-start"><h3>CSS</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-yellow fa-brands fa-js"></i></div>
+                            <div class="col-8 text-start"><h3>Javascript</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><img src="./assets/media/jquery_icon.png" width="40%" height="auto"/>
+                            </div>
+                            <div class="col-8 text-start"><h3>JQuery</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-indigo fa-brands fa-php"></i></div>
+                            <div class="col-8 text-start"><h3>PHP</h3></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-4">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><i class="fs-1 text-indigo fa-brands fa-bootstrap"></i></div>
+                            <div class="col-8 text-start"><h3>Bootstrap</h3></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php require_once 'footer.php'; ?>
